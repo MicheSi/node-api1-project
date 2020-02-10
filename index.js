6de0f9509edfbf0f0f34aede6d589ca48aea4fe4
mlazzell17@gmail.com <MicheSi>
2020-02-10 14:51:46 -0700

    added cors

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
-
+const cors = require('cors');
 const Data = require('./data/db');
 
 const server = express ();
 
 server.use(express.json());
+server.use(cors());
 
 server.get('/', (req, res) => {
     res.json({ hello: "Server running"})
