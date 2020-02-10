d6fbb7da56d9675c9c86f543de7ddde7d343539c
mlazzell17@gmail.com <MicheSi>
2020-02-10 14:32:10 -0700

    update user working

--- index.js
+++ index.js
@@ -69,5 +69,26 @@ server.delete('/api/users/:id', (req, res) => {
     })
 })
 
+// update user
+server.put('/api/users/:id', (req, res) => {
+    const {id} = req.params;
+    const updatedUser = req.body;
+
+    Data.update(id, updatedUser)
+    .then(data => {
+        if (!id) {
+            res.status(404).json ({ message: "The user with the specified ID does not exist." })
+        } else if (!updatedUser.name || !updatedUser.bio) {
+            res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
+        
+        } else {
+            res.status(200).json(data)
+        }
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({ errorMessage: "The user information could not be modified." })
+    })
+})
+
 const port = 5000;
 server.listen(port, () => console.log(`API on port ${port}`));
